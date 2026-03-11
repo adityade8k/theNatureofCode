fbe050bbf22b94ddf7284d25275a57546ca7dc04
Aditya <229662497+adityade8k@users.noreply.github.com>
2026-03-11 10:08:17 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const CAMERA_CONFIG = {
   // Treat these as "pan offsets" (translate values) in your current system:
   // transform = translate(pan.x, pan.y) scale(zoom)
   pointA: { x: 500, y: 800 },
-  pointB: { x: 250, y: -5800},
+  pointB: { x: 250, y: -5900},
 
   // Zoom settings
   viewer: {
